7c26b7cb49e66b8ff4e3dd90bda4c2cd34b098ec
Dalibaba <linkedavi@googlemail.com>
2022-06-20 10:28:25 +0200

    configure django logger

--- backend/sensor_backend/api/database_helper.py
+++ backend/sensor_backend/api/database_helper.py
@@ -4,8 +4,20 @@ import datetime
 
 class DatabaseHelper:
 
+    def add_reading(self, reading):
+        self.format_reading(reading)
+
+        # check if sensor exists
+        # if not insert sensor to database
+
+        return 0
+
     @staticmethod
-    def add_to_database(reading):
+    def format_reading(reading):
+        return 0
+
+    @staticmethod
+    def add():
         obj = SensorValue()
         obj.sensor = reading["device"]
         obj.time = datetime.datetime.now()
